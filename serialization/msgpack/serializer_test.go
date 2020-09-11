f80f1c4585f07707ff3a824c74540b168e73d59a
dzrry <dzrrymihael@gmail.com>
2020-09-11 18:14:15 +0700

    test msgpack serialization

--- serialization/msgpack/serializer_test.go
+++ serialization/msgpack/serializer_test.go
@@ -1,20 +1,20 @@
-package json
+package msgpack
 
 import (
-	"encoding/json"
 	"github.com/dzrry/dzurl/domain"
 	"github.com/stretchr/testify/assert"
+	"github.com/vmihailenco/msgpack"
 	"testing"
 	"time"
 )
 
-func TestJsonSerializer(t *testing.T) {
+func TestMsgpackSerializer(t *testing.T) {
 	serializer := Redirect{}
 
 	t.Run("Decode invalid data", func(t *testing.T) {
 		invalidRaw := []byte("raw-invalid-message-for-avito-tech")
 		_, err := serializer.Decode(invalidRaw)
-		assert.Equal(t, "serializer.Json.Decode: invalid character 'r' looking for beginning of value", err.Error())
+		assert.Equal(t, "serializer.Msgpack.Decode: msgpack: invalid code=72 decoding map length", err.Error())
 	})
 
 	t.Run("Decode valid data", func(t *testing.T) {
@@ -23,22 +23,23 @@ func TestJsonSerializer(t *testing.T) {
 			URL:       "https://start.avito.ru/tech",
 			CreatedAt: time.Now().Unix(),
 		}
-		raw, err := json.Marshal(rdct)
+		raw, err := msgpack.Marshal(rdct)
 		assert.Nil(t, err)
 		res, err := serializer.Decode(raw)
 		assert.Nil(t, err)
 		assert.Equal(t, rdct, res)
 	})
-	
+
 	t.Run("Encode valid value", func(t *testing.T) {
 		rdct := &domain.Redirect{
 			Key:       "avito-tech",
 			URL:       "https://start.avito.ru/tech",
 			CreatedAt: time.Now().Unix(),
 		}
-		raw, err := json.Marshal(rdct)
+		raw, err := msgpack.Marshal(rdct)
 		assert.Nil(t, err)
 		res, err := serializer.Encode(rdct)
 		assert.Equal(t, res, raw)
 	})
 }
+
